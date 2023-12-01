bbb781bfb10f4323c6fb39e3898d532d88ddc29b
Ai Mulyana <ai.mulyana@outlook.com>
2023-12-01 18:20:05 +0700

    Add Header and Footer Component

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import {StatusBar} from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {View} from 'react-native';
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welocme to Little Lemon</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#495E57',
+      }}
+    >
+      <LittleLemonHeader />
+      <LittleLemonFooter />
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/LittleLemonHeader.js
@@ -0,0 +1,35 @@
+import {View, Text} from 'react-native';
+
+const LittleLemonHeader = () => {
+  return (
+    <View
+      style={{
+        flex: 0.2,
+        backgroundColor: '#F4CE14',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 30,
+          color: 'black',
+        }}
+        numberOfLines={3}
+      >
+        Welcome to
+        <Text
+          style={{
+            fontWeight: 'bold',
+            marginLeft: 4,
+          }}
+        >
+          Little Lemon
+        </Text>
+      </Text>
+    </View>
+  );
+};
+
+export default LittleLemonHeader;
